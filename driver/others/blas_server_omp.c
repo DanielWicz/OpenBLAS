e68f84a2334e835e5ae1fd4058ddd0acc16fdaba
Daniel Wiczew <daniel.wiczew@gmail.com>
2025-12-09 11:17:23 +0100

    Revert "Fix OpenMP buffer shutdown crash; keep copy OMP fast path"
    This reverts commit 88a05cbd7924658f95ebaae875e1219ed1700041.

--- driver/others/blas_server_omp.c
+++ driver/others/blas_server_omp.c
@@ -71,16 +71,22 @@ int blas_server_avail = 0;
 int blas_omp_number_max = 0;
 /* 0 means “follow the OpenMP runtime default for the current level”.
  * A positive value is an explicit user override via openblas_set_num_threads_local(). */
-int blas_omp_threads_local = 1;
+int blas_omp_threads_local = 0;
 
 extern int openblas_omp_adaptive_env(void);
 
 static void * blas_thread_buffer[MAX_PARALLEL_NUMBER][MAX_CPU_NUMBER];
+
+typedef union {
 #ifdef HAVE_C11
-static atomic_bool blas_buffer_inuse[MAX_PARALLEL_NUMBER];
+  atomic_bool inuse;
 #else
-static _Bool blas_buffer_inuse[MAX_PARALLEL_NUMBER];
+  _Bool inuse;
 #endif
+  char padding[64];
+} blas_buffer_inuse_t;
+
+static blas_buffer_inuse_t blas_buffer_inuse[MAX_PARALLEL_NUMBER];
 
 static void adjust_thread_buffers(void) {
 
@@ -88,12 +94,7 @@ static void adjust_thread_buffers(void) {
 
   //adjust buffer for each thread
   for(i=0; i < MAX_PARALLEL_NUMBER; i++) {
-    for(j=0; j < blas_cpu_number; j++){
-      if(blas_thread_buffer[i][j] == NULL){
-        blas_thread_buffer[i][j] = blas_memory_alloc(2);
-      }
-    }
-    for(; j < MAX_CPU_NUMBER; j++){
+    for(j=blas_cpu_number; j < MAX_CPU_NUMBER; j++){
       if(blas_thread_buffer[i][j] != NULL){
         blas_memory_free(blas_thread_buffer[i][j]);
         blas_thread_buffer[i][j] = NULL;
@@ -322,12 +323,14 @@ static void exec_threads(int thread_num, blas_queue_t *queue, int buf_index){
   if ((sa == NULL) && (sb == NULL) && ((queue -> mode & BLAS_PTHREAD) == 0)) {
 
     pos= thread_num;
-    buffer = (buf_index != -1) ? blas_thread_buffer[buf_index][pos] : NULL;
+    if (buf_index != -1) {
+        buffer = blas_thread_buffer[buf_index][pos];
+    }
 
-    //fallback
+    //fallback or dynamic
     if(buffer==NULL) {
       buffer = blas_memory_alloc(2);
-      release_flag=1;
+      blas_thread_buffer[buf_index][pos] = buffer;
     }
 
     if (sa == NULL) {
@@ -465,10 +468,10 @@ int exec_blas(BLASLONG num, blas_queue_t *queue){
     for(i=0; i < MAX_PARALLEL_NUMBER; i++) {
 #ifdef HAVE_C11
       _Bool inuse = false;
-      if(atomic_compare_exchange_weak(&blas_buffer_inuse[i], &inuse, true)) {
+      if(atomic_compare_exchange_weak(&blas_buffer_inuse[i].inuse, &inuse, true)) {
 #else
-      if(blas_buffer_inuse[i] == false) {
-        blas_buffer_inuse[i] = true;
+      if(blas_buffer_inuse[i].inuse == false) {
+        blas_buffer_inuse[i].inuse = true;
 #endif
         buf_index = i;
         break;
@@ -481,9 +484,9 @@ int exec_blas(BLASLONG num, blas_queue_t *queue){
   exec_blas_internal(num, queue, buf_index);
 
 #ifdef HAVE_C11
-  atomic_store(&blas_buffer_inuse[buf_index], false);
+  atomic_store(&blas_buffer_inuse[buf_index].inuse, false);
 #else
-  blas_buffer_inuse[buf_index] = false;
+  blas_buffer_inuse[buf_index].inuse = false;
 #endif
 
   return 0;
